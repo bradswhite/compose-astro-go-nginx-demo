fb3a6c877c86f6cfc510b6ad9952ee5a59862d96
Brad White <bradscottwhite@gmail.com>
2024-08-21 17:37:09 -0400

    first commit

---
+++ backend/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+  "log"
+  "fmt"
+  "net/http"
+  "encoding/json"
+  "github.com/gorilla/mux"
+  "github.com/rs/cors"
+)
+
+func main() {
+  type GreetingType struct {
+    Id    int     `json:"id"`
+    Name  string  `json:"name"`
+  }
+
+  r := mux.NewRouter()
+
+  r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    data := GreetingType{ Id: 1, Name: "Docker/Go" }
+    jsonData, err := json.Marshal(data)
+    if err != nil {
+      log.Fatal("Error marshalling json data!")
+    }
+    w.Header().Set("Content-Type", "application/json")
+    w.Write(jsonData)
+    //w.Write([]byte("Docker"))
+  })
+
+  c := cors.New(cors.Options{
+    //AllowedOrigins: []string{"http://localhost:8080"},
+    AllowedMethods: []string{
+      http.MethodGet,
+      http.MethodPost,
+      http.MethodPut,
+      http.MethodPatch,
+      http.MethodDelete,
+      http.MethodOptions,
+      http.MethodHead,
+    },
+    AllowedHeaders: []string{"*"},
+    AllowCredentials: true,
+  })
+
+  handler := c.Handler(r)
+
+  fmt.Println("Server is starting at port 3000")
+  log.Fatal(http.ListenAndServe(":3000", handler))
+}
---
+++ frontend/src/components/LoadData.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  const getData = async (): Promise<string> => {
+    //const url = import.meta.env.PUBLIC_API_URL;
+    const url = '/api';
+    const res = await fetch(url, {
+      headers: {'Access-Control-Allow-Origin': '*'}
+    });
+    return await res.json();
+  };
+</script>
+
+{#await getData()}
+  <p>Loading data...</p>
+{:then data}
+  <p>Hello {data.name}!</p>
+{:catch error}
+  <p>Cannot load data</p>
+  <p>{error.message}</p>
+{/await}
